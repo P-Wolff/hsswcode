099d9d4eff356f8a069bb43f15bbb67f5afa81cd
P-Wolff <priscilawolff@hotmail.com.br>
2024-08-24 14:50:58 -0300

    alteracao para codar

--- src/pages/notfounding/index.jsx
+++ src/pages/notfounding/index.jsx
@@ -9,7 +9,7 @@ export default function NotFounding() {
         <div className='page-not-founding'>
             <div>
                 <h1 className='titulo'>Page Not Found</h1>
-                <img className='erro-img' src='/assets/images/404error.png' />
+                <img className='erro-img' src='/assets/images/404error.png' alt='imagem-error'/>
             </div>
         </div>
     );
