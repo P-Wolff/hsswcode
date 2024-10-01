c653dd8a8874b28e307bf1e71919a5abc6b905d9
P-Wolff <priscilawolff@hotmail.com.br>
2024-10-01 09:50:30 -0300

    arrumar tudo

--- src/components/cabecalho/index.jsx
+++ src/components/cabecalho/index.jsx
@@ -6,52 +6,78 @@ import Logo from '../../assets/land-page/logo.svg';
 
 
 export default function Cabecalho () {
-    const [ ativar, setAtivar ] = useState ( "nav__menu" );
-    const [ iconeMenu, setIconeMenu ] = useState ( "nav__Menu" );
-        const navMenu = () => {
-            ativar === "nav__menu" 
-            ? setAtivar( 'nav__menu nav__ativar' ) 
-            : setAtivar ( "nav__menu" );
 
-            //      Icone de Menu
+          
+        
 
-            iconeMenu === 'nav__Menu'
-            ? setIconeMenu ( "nav__Menu Menu" )
-            : setIconeMenu ( "nav__Menu" );
-        };
     
     return(
-        <div className='comp-cabecalho'>
-            <div class="cabecalho__container">
-                <header className='cabecalho' >
+        <div className="comp_cabecalho pagina">
+            <div className="pagina-nav">
 
-                    <nav className='navevagao'>
-                            <ul className={ativar}>
-                            <Link to='/'>
-                                <   img className='logo' src={Logo} alt='HSSW Code' title='HSSW Code'/>
-                            </Link>
-                                <Link to='/' className='nav__item'>Como fazemos</Link>
-                                <Link to='/' className='nav__item'>Jeito HSSW Code</Link>
-                                <Link to='/sobre' className='nav__item'>Sobre nós</Link>
-                                <Link to='/trabalho' className='nav__item'>Trabalhe conosco</Link>
-                                <Link to='/' className='nav__item'>FeedBacks</Link>
-                                <Link to='/contato' className='nav__item'>Contato</Link>
-                            </ul>
+                <div className="bar-grad"></div>
 
-                            <div onClick = {navMenu} className={iconeMenu}>
-                                <div className="linha1"></div> 
-                                <div className="linha2"></div> 
-                                <div className="linha3"></div>
-                            </div>
-                        </nav>
+                <nav className="bar-nav">
+                    <img src={Logo} alt='HSSW Code' title='HSSW Code' />
 
-                    <div id="secao__container">
-                        <span className="texto__container">HSSW Code</span>
-                        <span>A tecnologia e o mundo estão a evoluir, e você?</span>
+                    <div class="icone-menu" id="mobile-menu">
+                        <span class="bar"></span>
+                        <span class="bar"></span>
+                        <span class="bar"></span>
                     </div>
-                </header>
+
+                    <ul className="nav">
+                        
+                        <li className="nav_item"> 
+                            <Link to='/'>
+                                Como Fazemos 
+                            </Link> 
+                        </li>
+
+                        <li className="nav_item"> 
+                            <Link to='/'>
+                                Jeito HSSW Code 
+                            </Link> 
+                        </li>
+
+                        <li className="nav_item"> 
+                            <Link to='/'>
+                                Sobre Nós 
+                            </Link> 
+                        </li>
+
+                        <li className="nav_item"> 
+                            <Link to='/'>
+                                Trabalhe Conosco 
+                            </Link> 
+                        </li>
+
+                        <li className="nav_item"> 
+                            <Link to='/'>
+                                FeedBacks 
+                            </Link> 
+                        </li>
+
+                        <li className="nav_item"> 
+                            <Link to='/'>
+                                Contatos 
+                            </Link> 
+                        </li>
+
+                    </ul>
+                </nav>
+
             </div>
 
+
+            <div id="secao_container">
+                <span className="titulo">
+                    HSSW Code
+                </span>
+                <span className="texto">
+                    A tecnologia e o mundo estão a evoluir, e você?
+                </span>
+            </div>
         </div>
     );
 }
