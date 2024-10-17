1584d000adf03d3075dc9c5b63c96d98cda29d5c
P-Wolff <priscilawolff@hotmail.com.br>
2024-10-16 22:31:25 -0300

    alteracao footer... falta pouco

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -9,79 +9,67 @@ export default function Footer () {
     return (
         <div className='comp-footer pagina'>
             <footer className="footer">
+                <div className="sb__footer section__padding">
+                    <div className="sb__footer-links">
 
-                <div className="container">
-                    <div className="row">
+                        <div className="sb__footer-links-div">
+                            <img src={Logo} alt='HSSW Code' title='HSSW Code' className='logo' />
 
-                        <div className="explore" >
-                            <img src={Logo} alt='HSSW Code' title='HSSW Code'/>
-                            <div className="info">
+                            <h4> Pessoas e empresas que se 
+                                <br /> realizam nunca mais 
+                                <br />são as mesmas. 
+                                <br /><br /> HSSW Code.
+                                <br /> O que persiste é o aprendizado. 
+                                </h4>
+                        </div>
+                        
+                        <div className="sb__footer-links_div">
+                            <h4> Explore </h4>
+                            <Link className='foot__link' href="/"> Jeito HSSW Code </Link>
+                            <Link className='foot__link' href="/"> Como Fazemos </Link>
+                            <Link className='foot__link' href="/sobre"> Sobre nós </Link>
+                        </div>
+                        <div className="sb__footer-links_div">
+                            <h4></h4>
+                            <Link className='foot__link' href="/trabalho"> Trabalhe conosco </Link>
+                            <Link className='foot__link' href="/"> FeedBacks </Link>
+                            <Link className='foot__link' href="/contato"> Contato </Link>
+                        </div>
 
-                                <div className="info__text">
-                                    <p className="text-justify"> 
-                                        Pessoas e empresas que se realizam nunca mais são as mesmas.
-                                    </p>
-                                    <p className="text-justify"> 
-                                        HSSW Code. 
-                                        <br /> O que persiste é o aprendizado.
-                                    </p>
-                                </div>
 
+                        <div className="sb__footer-links_div">
+                            <h4>Em breve</h4>
+                            <div className="social-icons">
+                                <p><Link className="facebook" href="#"><i class="fa fa-facebook"></i></Link></p>
+                                <p><Link className="twitter" href="#"><i class="fa fa-twitter"></i></Link></p>
+                                <p><Link className="instagram" href="#"><i class="fa fa-instagram"></i></Link></p>
+                                <p><Link className="linkedin" href="#"><i class="fa fa-linkedin"></i></Link></p>
                             </div>
- 
                         </div>
 
-                        <div className="explore">
-                            <h6>Explore</h6>
-                            <ul className="footer-links">
-                                <li><Link className='foot__link' href="/"> Jeito HSSW Code </Link></li>
-                                <li><Link className='foot__link' href="/"> Como Fazemos </Link></li>
-                                <li><Link className='foot__link' href="/sobre"> Sobre nós </Link></li>
-                                <li><Link className='foot__link' href="/trabalho"> Trabalhe conosco </Link></li>
-                                <li><Link className='foot__link' href="/"> FeedBacks </Link></li>
-                                <li><Link className='foot__link' href="/contato"> Contato </Link></li>
-                            </ul>
+                        <div className="grad-bar"></div>
+
+                        <div className="sb_footer-below">
+                            <div className="sb__footer-copyright">
+                                <p>
+                                    @{new Date().getFullYear()} All Rights Reserved by HSSW Code.
+                                </p>
+                            </div>
+
+
+                            <div className="sb__footer-below-links">
+                                <Link className='foot__links' href="/termos"> Termos & Condições </Link>
+                                <Link className='foot__links' href="/privacidade"> Politica de Privacidade </Link>
+                                <Link className='foot__links' href="/security"> Segurança </Link>
+                                <Link className='foot__links' href="/cookie"> Declarações de Cookies </Link>
+                                <Link className='foot__links' href="/faq"> FAQ </Link>
+                            </div>
+
                         </div>
 
-                        <div className="explore">
-                            <ul className="footer-links">
-                                <li>
-                                    <Link className='foot__link' href="/"> 
-                                        Politica de Privacidade 
-                                    </Link>
-                                </li>
-                                <li>
-                                    <Link className='foot__link' href="/"> 
-                                        FAQ 
-                                    </Link>
-                                </li>
-                            </ul>
-                        </div>
                     </div>
 
-                    <div className="grad-bar"></div>
                 </div>
-
-
-                <div className="container">
-                    <div className="row">
-                        <div className="explore">
-                            <p className="copyright-text">Copyright &copy; 2024 All Rights Reserved by 
-                                <Link href="/">HSSW Code</Link>.
-                            </p>
-                        </div>
-
-                        <div className="explore">
-                            <ul className="social-icons">
-                                <li><Link className="facebook" href="#"><i class="fa fa-facebook"></i></Link></li>
-                                <li><Link className="twitter" href="#"><i class="fa fa-twitter"></i></Link></li>
-                                <li><Link className="instagram" href="#"><i class="fa fa-instagram"></i></Link></li>
-                                <li><Link className="linkedin" href="#"><i class="fa fa-linkedin"></i></Link></li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-
             </footer>
         </div>
 
