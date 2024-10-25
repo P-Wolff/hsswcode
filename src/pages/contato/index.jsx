d28b6edd08c175df1d86ec8ab49acba3f749b913
P-Wolff <priscilawolff@hotmail.com.br>
2024-10-24 22:05:27 -0300

    cards semiprontos num nivel de 70%...

--- src/pages/contato/index.jsx
+++ src/pages/contato/index.jsx
@@ -2,8 +2,6 @@ import './index.scss';
 import { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEnvelope, faPhone, faLocationDot, faClock } from '@fortawesome/free-solid-svg-icons';
-
-
 import Cabecalho from '../../components/cabecalho';
 import Footer from '../../components/footer';
 
@@ -19,14 +17,10 @@ export default function Contatos() {
     setActiveGroup(null);
   };
 
-
-
   return (
-    <div className="pagina-contato ">
+    <div className="pagina-contato">
       <Cabecalho />
-
       <h1>FALE CONOSCO</h1>
-
 
       <div className="container">
         <div className="box-contacts">
@@ -36,48 +30,32 @@ export default function Contatos() {
               <p>Descubra como as soluções e projetos da HSSW Code podem impulsionar o seu negócio</p>
             </div>
 
-            <div>&nbsp;</div>
-
-
             {activeGroup === "solucoes" && (
-              <div className="form formulario">
+              <div className="content-container">
+                <div className="form formulario">
                   <input type="text" placeholder='Nome completo*'/>
                   <input type="text" placeholder='E-mail Corporativo*'/>
                   <input type="text" placeholder='Celular*'/>
                   <input type="text" placeholder='Empresa*'/>
                   <input type="text" placeholder='Cargo*'/>
-
-                  <div>&nbsp;</div>
-
                   <p>Como podemos ajudar?</p>
                   <input type="text" />
-
-                  <div>&nbsp;</div>
-
                   <p>Ao informar meus dados, eu concordo em receber comunicações da empresa e com a Política de Privacidade</p>
                   <button className="back-button" onClick={handleBackButton}>Voltar</button>
                   <button> Enviar </button>
-
+                </div>
               </div>
-
             )}
           </div>
 
-
-          <div>&nbsp;</div>
-
-
           <div className={`carreira group ${activeGroup === "carreira" ? "active" : ""}`} onClick={(e) => { e.stopPropagation(); handleGroupClick("carreira"); }}>
-            
             <div className="texto">
               <p>Junte-se ao nosso time - Veja as oportunidades disponíveis e como fazer parte da HSSW Code</p>
             </div>
 
-
             {activeGroup === "carreira" && (
-              <div className="container-content">
-
-                <div className={`icons ${activeGroup === "outros" ? "active" : ""}`}>
+              <div className="content-container">
+                <div className="icons">
                   <FontAwesomeIcon icon={faEnvelope} /> recrutamento@hsswcode.com.br<br />
                   <FontAwesomeIcon icon={faPhone} /> +55 (11) 2118-1615<br />
                   <FontAwesomeIcon icon={faLocationDot} /> São Paulo | São Paulo<br />
@@ -85,11 +63,10 @@ export default function Contatos() {
                 </div>
 
                 <div className="form formulario">
-                  <input type="text" placeholder='Nome completo*'/> &nbsp;
-                  <input type="text" placeholder='E-mail Corporativo*'/> &nbsp;
-                  <input type="text" placeholder='Celular*'/> &nbsp;
-                  <input type="text" placeholder='Assunto*'/> &nbsp;
-
+                  <input type="text" placeholder='Nome completo*'/>
+                  <input type="text" placeholder='E-mail Corporativo*'/>
+                  <input type="text" placeholder='Celular*'/>
+                  <input type="text" placeholder='Assunto*'/>
                   <select>
                     <option>Como nos conheceu?</option>
                     <option>Indicação</option>
@@ -99,94 +76,29 @@ export default function Contatos() {
                     <option>Site</option>
                     <option>Outro</option>
                   </select>
-
-                  <div>&nbsp;</div>
-                  
                   <p>Como podemos ajudar?</p>
                   <input type="text" />
-
-                  <div>&nbsp;</div>
-
                   <p>Ao informar meus dados, eu concordo em receber comunicações da empresa e com a Política de Privacidade</p>
                   <button className="back-button" onClick={handleBackButton}>Voltar</button>
                   <button> Enviar </button>
                 </div>
-
-            </div>
-
+              </div>
             )}
-
           </div>
-
-
-          <div>&nbsp;</div>
-
 
           <div className={`fornecedor group ${activeGroup === "fornecedor" ? "active" : ""}`} onClick={(e) => { e.stopPropagation(); handleGroupClick("fornecedor"); }}>
             <div className="texto">
               <p>Seja um fornecedor da HSSW Code, preencha as informações sobre seu negócio.</p>
             </div>
 
-
             {activeGroup === "fornecedor" && (
-              <div className="form formulario">
-                <input type="text" placeholder='Nome completo*'/> &nbsp;
-                <input type="text" placeholder='E-mail Corporativo*'/> &nbsp;
-                <input type="text" placeholder='Celular*'/> &nbsp;
-                <input type="text" placeholder='Empresa*'/> &nbsp;
-
-                <select>
-                  <option>Como nos conheceu?</option>
-                  <option>Indicação</option>
-                  <option>Google</option>
-                  <option>LinkedIn</option>
-                  <option>Instagram</option>
-                  <option>Site</option>
-                  <option>Outro</option>
-                </select>
-
-                <div>&nbsp;</div>
-                  
-                <p>Como podemos ajudar?</p>
-                <input type="text" />
-
-                <div>&nbsp;</div>
-
-                <p>Ao informar meus dados, eu concordo em receber comunicações da empresa e com a Política de Privacidade</p>
-                <button className="back-button" onClick={handleBackButton}>Voltar</button>
-                <button> Enviar </button>
-              </div>
-            )}
-          </div>
-
-
-          <div>&nbsp;</div>
-
-
-          <div className={`outros group ${activeGroup === "outros" ? "active" : ""}`} onClick={(e) => { e.stopPropagation(); handleGroupClick("outros"); }}>
-            
-            <div className="texto">
-              <p>Tem dúvidas, sugestões, feedbacks ou deseja falar sobre outros assuntos? Contate-nos por aqui.</p>
-            </div>
-
-
-            {activeGroup === "outros" && (
-
-              <div className="container-content">
-
-                <div className={`icons ${activeGroup === "outros" ? "active" : ""}`}>
-                  <FontAwesomeIcon icon={faEnvelope} /> recrutamento@hsswcode.com.br<br />
-                  <FontAwesomeIcon icon={faPhone} /> +55 (11) 2118-1615<br />
-                  <FontAwesomeIcon icon={faLocationDot} /> São Paulo | São Paulo<br />
-                  <FontAwesomeIcon icon={faClock} /> 09:00 - 17:00
-                </div>
+              <div className="content-container">
 
                 <div className="form formulario">
-                  <input type="text" placeholder='Nome completo*'/> &nbsp;
-                  <input type="text" placeholder='E-mail Corporativo*'/> &nbsp;
-                  <input type="text" placeholder='Celular*'/> &nbsp;
-                  <input type="text" placeholder='Assunto*'/> &nbsp;
-
+                  <input type="text" placeholder='Nome completo*'/>
+                  <input type="text" placeholder='E-mail Corporativo*'/>
+                  <input type="text" placeholder='Celular*'/>
+                  <input type="text" placeholder='Empresa*'/>
                   <select>
                     <option>Como nos conheceu?</option>
                     <option>Indicação</option>
@@ -196,32 +108,88 @@ export default function Contatos() {
                     <option>Site</option>
                     <option>Outro</option>
                   </select>
-
-                  <div>&nbsp;</div>
-                  
                   <p>Como podemos ajudar?</p>
                   <input type="text" />
-
-                  <div>&nbsp;</div>
-
                   <p>Ao informar meus dados, eu concordo em receber comunicações da empresa e com a Política de Privacidade</p>
                   <button className="back-button" onClick={handleBackButton}>Voltar</button>
                   <button> Enviar </button>
                 </div>
-
               </div>
+            )}
+          </div>
 
+          <div className={`outros group ${activeGroup === "outros" ? "active" : ""}`} onClick={(e) => { e.stopPropagation(); handleGroupClick("outros"); }}>
+            <div className="texto">
+              <p>Tem dúvidas, sugestões, feedbacks ou deseja falar sobre outros assuntos? Contate-nos por aqui.</p>
+            </div>
+
+            {activeGroup === "outros" && (
+              <div className="content-container">
+                <div className="icons">
+                  <FontAwesomeIcon icon={faEnvelope} /> contato@hsswcode.com.br<br />
+                  <FontAwesomeIcon icon={faPhone} /> +55 (11) 2118-1617<br />
+                  <FontAwesomeIcon icon={faLocationDot} /> São Paulo | São Paulo<br />
+                  <FontAwesomeIcon icon={faClock} /> 09:00 - 17:00
+                </div>
+
+                <div className="form formulario">
+                  <input type="text" placeholder='Nome completo*'/>
+                  <input type="text" placeholder='E-mail Corporativo*'/>
+                  <input type="text" placeholder='Celular*'/>
+                  <input type="text" placeholder='Assunto*'/>
+                  <select>
+                    <option>Como nos conheceu?</option>
+                    <option>Indicação</option>
+                    <option>Google</option>
+                    <option>LinkedIn</option>
+                    <option>Instagram</option>
+                    <option>Site</option>
+                    <option>Outro</option>
+                  </select>
+                  <p>Como podemos ajudar?</p>
+                  <input type="text" />
+                  <p>Ao informar meus dados, eu concordo em receber comunicações da empresa e com a Política de Privacidade</p>
+                  <button className="back-button" onClick={handleBackButton}>Voltar</button>
+                  <button> Enviar </button>
+                </div>
+              </div>
             )}
           </div>
 
         </div>
-          
       </div>
-        
+
+      <div className="random-text">
+          <div className="other-text">
+            <h1> Sala de Imprensa </h1>
+            <p>Vamos trabalhar lado a lado?
+              <br /> Entre em contato com o nosso time.
+            </p>
+          </div>
+
+          <div className="box-overflow">
+            <div className="paragrafo">
+              <p>HSSW Code Comunicações</p>
+            </div>
+            
+            <div className="emails">
+              <p> Priscila Wolff
+                <br />priscila@hsswcode.com.br
+              </p>
+
+              <p> Kevilyn Sandes
+                <br />kevilyn@hsswcode.com.br
+              </p>
+
+            </div>
+
+          </div>
+
+      </div>
+
+
+
       <Footer />
-  
     </div>
-  
   );
 }
-
